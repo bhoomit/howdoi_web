e212315cbfe9c4bce27b0603365e0624f9a00d99
Bhoomit Vasani <bhoomitvasani@mac.local>
2013-01-18 01:08:54 +0530

    init

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+import urllib, json, traceback
+from functools import wraps
+from flask import Flask, redirect, request, current_app, jsonify
+from howdoi import howdoi
+
+app = Flask(__name__)
+
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f().data) + ')'
+            return current_app.response_class(content, mimetype='application/json')
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/howdoi', methods=['GET','POST'])
+@support_jsonp
+def hello():
+    try:
+        query = urllib.quote_plus(request.args.get('q'))
+        result = howdoi.get_instructions({'query': query})
+    except Exception as e:
+        print traceback.format_exc()
+        return jsonify({'status':'error','msg': e.args ,'traceball':traceback.format_exc()})
+
+    return result
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
